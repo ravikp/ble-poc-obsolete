c89826783a4ec281709f1f18b9c503eb7694ad60
krishnakumar4a4 <krishna.thokala2010@gmail.com>
2022-11-08 17:45:06 +0530

    [Ravi | Krishna] | Integrate jwtsign into ios project

--- ios/rusty-ios/rusty-ios/ContentView.swift
+++ ios/rusty-ios/rusty-ios/ContentView.swift
@@ -27,5 +27,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 func getName() -> String {
-    return sprinkle(input: "Ravi")
+    let private_key: [UInt8] = [176, 248, 152, 2, 121, 212, 223, 159, 56, 59, 253, 110, 153, 11, 69, 197, 252, 186, 28, 79, 190, 247, 108, 39, 185, 20, 29, 255, 80, 185, 121, 131, 252, 109, 239, 156, 33, 22, 23, 4, 81, 188, 242, 194, 118, 63, 169, 22, 150, 209, 33, 73, 23, 89, 145, 242, 38, 179, 77, 219, 226, 50, 225, 167];
+    return jwtsign(privateKey: private_key, claims: "test-claims")
+//    return sprinkle(input: "Ravi")
 }
